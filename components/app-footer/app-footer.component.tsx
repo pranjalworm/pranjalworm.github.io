fdc5abe5bc5715ad01566cee31dc8c2959e05945
Pranjal Dubey <pranjalworm@gmail.com>
2022-11-01 12:03:46 +0530

    deleted social media links component

--- components/app-footer/app-footer.component.tsx
+++ components/app-footer/app-footer.component.tsx
@@ -1,16 +1,7 @@
-import SocialMediaLinks from '../social-media-links/social-media-links.component'
-
 const AppFooter = () => {
-
   return (
-    <footer className='text-gray-500 bg-gray-100 text-center p-8 mt-24 dark:bg-gray-900 dark:text-white'>
-
-      <SocialMediaLinks />
-
-      <div className='mt-6'>
-        © Pranjal Dubey
-      </div>
-
+    <footer className='text-gray-500 bg-gray-100 text-center p-6 mt-24 dark:bg-gray-900 dark:text-white'>
+      © Pranjal Dubey
     </footer>
   )
 }
